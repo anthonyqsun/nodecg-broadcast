2e9956cc1b4d5bba757b0a7c3da83136fbe945b1
anthonyqsun <qsun30@stuy.edu>
2023-12-01 02:56:18 -0500

    fixed bugs, added map-list panel

--- nodecg/bundles/holiday-classic/extension.js
+++ nodecg/bundles/holiday-classic/extension.js
@@ -8,44 +8,59 @@ class Player {
     }
 }
 
+let allMaps = {
+    "control": {
+        "Busan": "FILE/LOCATION",
+    },
+    "escort": {
+        "Circuit Royal": "FILE/LOCATION",
+    },
+    "hybrid": {
+        "Blizzard World": "FILE/LOCATION",
+    },
+    "push": {
+        "Colosseo": "FILE/LOCATION",
+    }
+}
+
 // TEAM DEFINITIONS
 let teams = {
-    "alpha": [
+    "Alpha": [
         new Player("a-alpha", "they/them", "offense", ""),
         new Player("b-alpha", "they/them", "offense", ""),
         new Player("c-alpha", "they/them", "tank", ""),
         new Player("d-alpha", "they/them", "support", ""),
         new Player("e-alpha", "they/them", "support", ""),
     ],
-    "bravo": [
+    "Bravo": [
         new Player("a-bravo", "they/them", "offense", ""),
         new Player("b-bravo", "they/them", "offense", ""),
         new Player("c-bravo", "they/them", "tank", ""),
         new Player("d-bravo", "they/them", "support", ""),
         new Player("e-bravo", "they/them", "support", ""),
     ],
-    "charlie": [
+    "Charlie": [
         new Player("a-charlie", "they/them", "offense", ""),
         new Player("b-charlie", "they/them", "offense", ""),
         new Player("c-charlie", "they/them", "tank", ""),
         new Player("d-charlie", "they/them", "support", ""),
         new Player("e-charlie", "they/them", "support", ""),
     ],
-    "delta": [
+    "Delta": [
         new Player("a-delta", "they/them", "offense", ""),
         new Player("b-delta", "they/them", "offense", ""),
         new Player("c-delta", "they/them", "tank", ""),
         new Player("d-delta", "they/them", "support", ""),
         new Player("e-delta", "they/them", "support", ""),
     ],
-    "echo": [
+    "Echo": [
         new Player("a-echo", "they/them", "offense", ""),
         new Player("b-echo", "they/them", "offense", ""),
         new Player("c-echo", "they/them", "tank", ""),
         new Player("d-echo", "they/them", "support", ""),
         new Player("e-echo", "they/them", "support", ""),
     ],
-    "foxtrot": [
+    "Foxtrot": [
         new Player("a-foxtrot", "they/them", "offense", ""),
         new Player("b-foxtrot", "they/them", "offense", ""),
         new Player("c-foxtrot", "they/them", "tank", ""),
@@ -57,10 +72,13 @@ let teams = {
 module.exports = function (nodecg) {
     const teamRep = nodecg.Replicant("teams");
     teamRep.value = teams;
-    console.log("executing");
-    nodecg.log.info("executing");
+
+    const mapRep = nodecg.Replicant("allMaps");
+    mapRep.value = allMaps;
+
     // OTHER REPLICANT INITIALIZATIONS
     nodecg.Replicant("left");
     nodecg.Replicant("right");
+    nodecg.Replicant("currentMaps");
 }
 
